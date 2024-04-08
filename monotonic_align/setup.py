65c477cad5a359295807c3b67483bdfeb0af48d3
dariadiatlova <dari.diatlova@gmail.com>
2024-04-08 22:31:43 +0300

    colab training

--- monotonic_align/setup.py
+++ monotonic_align/setup.py
@@ -5,6 +5,6 @@ from Cython.Build import cythonize
 
 setup(
     name="monotonic_align",
-    ext_modules=cythonize("/app/monotonic_align/core.pyx"),
+    ext_modules=cythonize("/content/MB-iSTFT-VITS2/monotonic_align/core.pyx"), #/app/monotonic_align/core.pyx if docker
     include_dirs=[numpy.get_include()],
 )
